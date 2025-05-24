e389984898d7f65e8c9c0205b06a1006f2c0936f
sohayeb01 <gasmisohayeb74@gmail.com>
2025-05-24 19:31:29 +0000

    Update auth.php

--- tp10/config/auth.php
+++ tp10/config/auth.php
@@ -1,14 +1,12 @@
 <?php
 // Authentication configuration
-define('USERLOGIN', 'itisme');
-define('USERPASS', 'justme');
+define('ADMINLOGIN', 'itisme');
+define('ADMINPASS', 'justme');
 
-// Admin user for database management
-define('ADMINLOGIN', 'admin');
-define('ADMINPASS', 'admin123');
+
 
 // List of valid users
 $VALID_USERS = [
-    ['login' => USERLOGIN, 'password' => USERPASS, 'role' => 'user'],
-    ['login' => ADMINLOGIN, 'password' => ADMINPASS, 'role' => 'admin']
+    ['login' => ADMINLOGIN, 'password' => ADMINPASS, 'role' => 'admin'],
+
 ]; 
